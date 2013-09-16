8fe18b4493155d12f1f0d2143f061c13e96d77bd
han <hgk617@naver.com>
2013-09-16 12:39:09 +0000

    helper 세팅...

--- spec/helpers/home_helper_spec.rb
+++ spec/helpers/home_helper_spec.rb
@@ -1,15 +1,19 @@
-require 'spec_helper'
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+require 'capybara'
+require 'capybara/rspec'
 
-# Specs in this file have access to a helper object that includes
-# the HomeHelper. For example:
-#
-# describe HomeHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
-describe HomeHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
+RSpec.configure do |config|
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_base_class_for_anonymous_controllers = false
+  config.order = "random"
+  config.include Capybara::DSL
 end
