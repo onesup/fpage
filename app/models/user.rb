334b37614ebf3edb6687a7702f4245d1a0b137dd
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2013-09-16 10:14:22 +0900

    facebook login 완료

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :confirmable, :lockable, :timeoutable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, 
+  :validatable, :omniauthable, :omniauth_providers => [:facebook]
+  
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create(name:auth.extra.raw_info.name,
+                           provider:auth.provider,
+                           uid:auth.uid,
+                           email:auth.info.email,
+                           password:Devise.friendly_token[0,20]
+                           )
+    end
+    user
+  end
+  
 end
