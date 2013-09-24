b9edcb999a652b969d52b034f84e6bbe2bf2e35a
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2013-09-25 05:53:15 +0900

    Page 모델 더 보기좋게 리팩토링

--- app/models/page.rb
+++ app/models/page.rb
@@ -7,20 +7,6 @@ class Page < ActiveRecord::Base
   serialize :location
   serialize :cover
   serialize :picture
-
-  def self.test(user_token)
-    page_objects = Page.fetch_page_list(user_token)
-    page_objects.each do |page_object|
-      Page.create page_object
-      # fields = [:name, :link, :category_list, :is_published, :can_post, :likes, :location, :phone, :checkins, :picture, :cover, :website, :description, :unread_message_count, :unread_notif_count, :unseen_message_count, :about, :description_html, :talking_about_count, :global_brand_parent_page, :access_token, :hours]
-      # if exists?(uid: page_object["id"]).nil?
-      #   fields.each do |field|
-      #     page = Page.new
-      #     page.send(field.to_s+'=',page_object.send(field.to_s)
-      #   end
-      # end
-    end
-  end
     
   def self.fetch_page_list(user_token)
     graph = Koala::Facebook::API.new(user_token)
@@ -28,11 +14,11 @@ class Page < ActiveRecord::Base
     page_ids = Array.new
     pages.each{|page| page_ids << page["id"]}
     page_objects = Array.new
+    fields = "name, link, category_list, is_published, can_post, likes, location, phone, checkins, picture, 
+      cover, website, description, unread_message_count, unread_notif_count, unseen_message_count, about, 
+      description_html, talking_about_count, global_brand_parent_page, access_token, hours"
     page_ids.each do |page_id|
-      page_objects << future{ graph.get_object(page_id, {"fields" => "name, link, category_list, is_published, can_post, 
-                                              likes, location, phone, checkins, picture, cover, website, description,
-                                              unread_message_count, unread_notif_count, unseen_message_count, about,
-                                              description_html, talking_about_count, global_brand_parent_page, access_token, hours"})}
+      page_objects << future{ graph.get_object(page_id,{"fields" => fields}) }
     end
     page_objects
   end
@@ -43,13 +29,9 @@ class Page < ActiveRecord::Base
       page_id = page_object["id"]
       page_object["uid"] = page_object["id"]
       if exists?(uid: page_id).nil?
-        Page.create!(
-          page_object
-        ) 
+        Page.create!(page_object) 
       else
-        find_by_uid(page_id).update!(
-          page_object
-        )
+        find_by_uid(page_id).update!(page_object)
       end
     end
     
